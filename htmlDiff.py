72bdfa5c4b9667b1e3f6ff29eb7701d6a5449850
Gabriel Albino <enggabrielalbino@gmail.com>
2022-04-15 22:03:04 -0300

    Adicionando view do diff

--- htmlDiff.py
+++ htmlDiff.py
@@ -1,29 +1,39 @@
 from dataclasses import dataclass
+import webbrowser
+
 
 @dataclass
 class Addition:
-    """Classe generica apenas para indicar uma adição"""
-    value: str
-    def __init__(self, value):
-        self.value = value
+  """Classe generica apenas para indicar uma adição"""
+  value: str
+  def __init__(self, value):
+    self.value = value
 
 
 @dataclass
 class Removal:
-    """Classe generica apenas para indicar uma adição"""
-    value: str
-    def __init__(self, value):
-        self.value = value
+  """Classe generica apenas para indicar uma adição"""
+  value: str
+  def __init__(self, value):
+    self.value = value
 
 
 @dataclass
 class Unchanged:
-    """Classe generica apenas para indicar uma adição"""
-    value: str
-    def __init__(self, value):
-        self.value = value
+  """Classe generica apenas para indicar uma adição"""
+  value: str
+  def __init__(self, value):
+    self.value = value
 
+def createFile(content):
+  with open('result.html', 'w') as file:
+    file.write(content)
+    file.close();
 
+def openFileInBrowser():
+  print("Se nada acontecer, mude o caminho do arquivo no método 'openFileInBrowser'");
+  filename = '/Users/albino/Documents/unb/htmlDiff/result.html'
+  webbrowser.open_new_tab(filename)
 
 def get_lcs_len(text1, text2):
     n = len(text1)
@@ -76,7 +86,25 @@ def find_lcs_string(text1, text2):
   return list(reversed(results))
 
 
-diff = find_lcs_string("asdfaaa", "afdsbaa")
 
-for char in diff:
-    print(char)
+def diffToHtml(diff):
+  diffHtml = ""
+  lastClass = ""
+  for char in diff:
+    if lastClass != type(char).__name__:
+      if lastClass != "":
+        diffHtml += "</div>"
+      diffHtml += f"<div class=\"{type(char).__name__}\">{char.value}";    
+      lastClass = type(char).__name__
+    else:
+      diffHtml += char.value
+  if len(diffHtml) > 0:
+    diffHtml += "</div>"
+  css = "<style>.diff > div{border: solid #333;border-width: 1px 1px 0px 1px; line-heigth: 18px} .diff{border-bottom: 1px solid black;}.Addition{background: green}.Addition::before{content:' + '}.Removal{background: red}.Removal::before{content:' - ';font-size:18px}.Unchanged{background: #888}</style>"
+  html = f"<html><head>{css}</head><body><div class=\"diff\">{diffHtml}</div></body></html>"
+  return html
+
+diff = find_lcs_string("asdfaaa", "afdsbaa")
+htmlContent = diffToHtml(diff)
+createFile(htmlContent)
+openFileInBrowser()
